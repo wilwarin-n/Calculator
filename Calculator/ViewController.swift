d7cd8a670cf13521bce1f0b46771efa881dbe0f3
Rumeysa <michiru.aware.lrn@gmail.com>
2024-07-16 12:39:25 +0300

    Create calculator project

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var numLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func numbers(){
+        
+    }
 
-
+    
 }
 
