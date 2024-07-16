a0e24d6daff7fa089d48b4f0f0d01880c93ceb40
Rumeysa <michiru.aware.lrn@gmail.com>
2024-07-16 15:00:59 +0300

    Added button functions

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -16,16 +16,65 @@ class ViewController: UIViewController {
     var performMath = false
     var operation = 0
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    
     
     
     @IBAction func numbers(_ sender: UIButton){
         if performMath == true {
             numLabel.text = String(sender.tag-1)
+            currentNum = Double(numLabel.text!)!
+            performMath = false
+        } else {
+            numLabel.text = numLabel.text! + String(sender.tag-1)
+            currentNum = Double(numLabel.text!)!
+            
         }
+        
+        
+    }
+    
+    @IBAction func buttons(_ sender: UIButton){
+        
+        if numLabel.text != "" && sender.tag != 11 && sender.tag != 16{
+            
+            preNum = Double(numLabel.text!)!
+            
+            if sender.tag == 12 {
+                numLabel.text = "/"
+            } else if sender.tag == 13{
+                numLabel.text = "*"
+            } else if sender.tag == 14{
+                numLabel.text = "-"
+            } else if sender.tag == 15{
+                numLabel.text = "+"
+            }
+            
+            operation = sender.tag
+            performMath = true
+            
+        } else if sender.tag == 16{
+            if operation == 12{
+                numLabel.text = String(preNum / currentNum)
+            }else if operation == 13{
+                numLabel.text = String(preNum * currentNum)
+            }else if operation == 14{
+                numLabel.text = String(preNum - currentNum)
+            }else if operation == 15{
+                numLabel.text = String(preNum + currentNum)
+            }
+            
+        }else if sender.tag == 11{
+            currentNum = 0
+            preNum = 0
+            numLabel.text = ""
+            operation = 0
+            }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
     }
 
     
